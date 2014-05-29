71617c2570a6dc882b34b545d04deda982b61df9
C2Tao <b97901182@gmail.com>
2014-05-30 03:05:41 +0800

    show puc

--- dnn_test.py
+++ dnn_test.py
@@ -2,6 +2,8 @@
 import numpy as np
 #from dnn import nn_layer
 import cPickle, gzip, numpy
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 # Load the dataset
 f = gzip.open('mnist.pkl.gz', 'rb')
@@ -10,5 +12,8 @@ f.close()
 
 print np.shape(train_set)
 print np.shape(valid_set)
-print train_set[0][0]
 print np.shape(test_set)
+
+print train_set[1][264]
+imgplot = plt.imshow(train_set[0][264].reshape(28,28))
+plt.show()
